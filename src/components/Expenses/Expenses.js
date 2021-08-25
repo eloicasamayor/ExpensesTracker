3eaa7af522dff3924e7f5c453e7900214cfff50a
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2021-08-25 20:47:37 +0200

    no expenses message added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,12 +10,18 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map(expense => 
+      {filteredExpenses.length === 0 
+      ? <p>No expenses found</p>
+      : filteredExpenses.map(expense => 
       <ExpenseItem 
         key={expense.id}
         title={expense.title} 
