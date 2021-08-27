d9cce839799b508e8f2ff4cd2b48c71456f63397
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2021-08-27 20:59:10 +0200

    input form bug fixed

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ const App = () => {
   }
   return (
     <div>
-      <h2>Let's get started!</h2>
       <NewExpense onSavedExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
